ece277ca69ba55b07dd8ac1fd656ef81c13d9378
Bogdan <bogdashka04@gmail.com>
2019-12-08 19:22:58 +0300

    Выводит назвнание, автора песни, и задник при игре

--- Menu.py
+++ Menu.py
@@ -5,13 +5,17 @@ class Menu:
     def __init__(self, screen):
         self.screen = screen
         self.maps = [['Christopher Larkin', 'Hornet', 'Files_of_map/map_img.png'],
-                     ['Christopher Larkin', 'hornet', 'Files_of_map/image.png']]
+                     ['zxczxc', 'ZXCzxccZXcSDcds', 'Files_of_map/image.png']]
 
     def render(self, what_in_focus, width, height):
         for i in self.maps:
             if self.maps.index(i) == what_in_focus:
                 self.screen.blit(pygame.image.load(i[2]), [0, 0])
                 pygame.draw.rect(self.screen, (32, 21, 94), [width - 600, height // 2.4, 600, 200])
+                pygame.draw.rect(self.screen, (255, 255, 255), [width - 600, height // 2.4, 600, 200], 1)
                 font = pygame.font.Font(None, 100)
-                text = font.render(i[1], 1, (0, 0, 0))
+                text = font.render(i[1], 1, (255, 255, 255))
                 self.screen.blit(text, [width - 580, height // 2 - 40])
+                font = pygame.font.Font(None, 40)
+                text = font.render(i[0], 1, (255, 255, 255))
+                self.screen.blit(text, [width - 540, height // 2 + 40])
