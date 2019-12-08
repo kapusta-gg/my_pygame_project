38b983da9608cc1150445bf42cef36b795be845c
Bogdan <bogdashka04@gmail.com>
2019-12-08 18:48:18 +0300

    Созданы условия для карты в фокусе

--- Menu.py
+++ Menu.py
@@ -2,9 +2,13 @@ import pygame
 
 
 class Menu:
-    def __init__(self, what_in_focus, screen):
-        self.wif = what_in_focus
+    def __init__(self, screen):
         self.screen = screen
+        self.maps = [['Christopher Larkin', 'hornet', 'Files_of_map/map_img.png'],
+                     ['Christopher Larkin', 'hornet', 'Files_of_map/image.png']]
 
-    def render(self):
-        pass
+    def render(self, what_in_focus, width, height):
+        for i in self.maps:
+            if self.maps.index(i) == what_in_focus:
+                self.screen.blit(pygame.image.load(i[2]), [0, 0])
+                pygame.draw.rect(self.screen, (76, 88, 102), [width - 600, height // 2.4, 600, 200])
