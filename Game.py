7ab0c662bf9820cbf46cac5ca2b028fc5a67f5c6
Bogdan <bogdashka04@gmail.com>
2019-12-08 19:04:47 +0300

    Можно переключаться между песнями

--- Game.py
+++ Game.py
@@ -9,6 +9,7 @@ screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
 back_pos = [0, 0]
 back_img = pygame.image.load('Files_of_map/map_img.png')
 what_visible = 'menu'
+map__in_focus = 0
 
 
 def play_music(music_path):
@@ -23,9 +24,15 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 5:
+                map__in_focus += 1
+            if event.button == 4:
+                map__in_focus -= 1
         if what_visible == 'menu':
-            b = Menu(0, screen)
-            b.render()
+            screen.fill((0, 0, 0))
+            b = Menu(screen)
+            b.render(map__in_focus, width, height)
         else:
             screen.blit(back_img, back_pos)
         pygame.display.flip()
--- Menu.py
+++ Menu.py
@@ -4,11 +4,14 @@ import pygame
 class Menu:
     def __init__(self, screen):
         self.screen = screen
-        self.maps = [['Christopher Larkin', 'hornet', 'Files_of_map/map_img.png'],
+        self.maps = [['Christopher Larkin', 'Hornet', 'Files_of_map/map_img.png'],
                      ['Christopher Larkin', 'hornet', 'Files_of_map/image.png']]
 
     def render(self, what_in_focus, width, height):
         for i in self.maps:
             if self.maps.index(i) == what_in_focus:
                 self.screen.blit(pygame.image.load(i[2]), [0, 0])
-                pygame.draw.rect(self.screen, (76, 88, 102), [width - 600, height // 2.4, 600, 200])
+                pygame.draw.rect(self.screen, (32, 21, 94), [width - 600, height // 2.4, 600, 200])
+                font = pygame.font.Font(None, 100)
+                text = font.render(i[1], 1, (0, 0, 0))
+                self.screen.blit(text, [width - 580, height // 2 - 40])
