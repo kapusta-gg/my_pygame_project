b6eeda7e13994f2885151370507259aa4388f6b6
Bogdan <bogdashka04@gmail.com>
2019-12-08 19:42:41 +0300

    Откредактировал Menu.py и Game.py

--- Game.py
+++ Game.py
@@ -6,10 +6,10 @@ pygame.init()
 
 size = width, height = GetSystemMetrics(0), GetSystemMetrics(1)
 screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
-back_pos = [0, 0]
-back_img = pygame.image.load('Files_of_map/map_img.png')
 what_visible = 'menu'
 map__in_focus = 0
+maps = [['Christopher Larkin', 'Hornet', 'Files_of_map/map_img.png', 'Files_of_map/map_music.mp3'],
+        ['zxczxc', 'ZXCzxccZXcSDcds', 'Files_of_map/image.png', 'Files_of_map/map_music.mp3']]
 
 
 def play_music(music_path):
@@ -25,16 +25,21 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 5:
-                map__in_focus += 1
-            if event.button == 4:
-                map__in_focus -= 1
+            if what_visible == 'menu':
+                if event.button == 5:
+                    map__in_focus += 1
+                if event.button == 4:
+                    map__in_focus -= 1
         if what_visible == 'menu':
             screen.fill((0, 0, 0))
-            b = Menu(screen)
-            b.render(map__in_focus, width, height)
+            b = Menu(screen, maps)
+            if map__in_focus < 0:
+                map__in_focus = 0
+            elif map__in_focus > len(maps) - 1:
+                map__in_focus = len(maps) - 1
+            a = b.render(map__in_focus, width, height)
         else:
-            screen.blit(back_img, back_pos)
+            pass
         pygame.display.flip()
 
 pygame.quit()
--- Menu.py
+++ Menu.py
@@ -2,17 +2,17 @@ import pygame
 
 
 class Menu:
-    def __init__(self, screen):
+    def __init__(self, screen, maps):
         self.screen = screen
-        self.maps = [['Christopher Larkin', 'Hornet', 'Files_of_map/map_img.png'],
-                     ['zxczxc', 'ZXCzxccZXcSDcds', 'Files_of_map/image.png']]
+        self.maps = maps
 
     def render(self, what_in_focus, width, height):
         for i in self.maps:
             if self.maps.index(i) == what_in_focus:
                 self.screen.blit(pygame.image.load(i[2]), [0, 0])
                 pygame.draw.rect(self.screen, (32, 21, 94), [width - 600, height // 2.4, 600, 200])
-                pygame.draw.rect(self.screen, (255, 255, 255), [width - 600, height // 2.4, 600, 200], 1)
+                pygame.draw.rect(self.screen, (255, 255, 255), [width - 600, height // 2.4, 600, 200], 4)
+                pygame.draw.circle(self.screen, (32, 21, 94), [width - 600, int(height // 2.4) + 100], 100)
                 font = pygame.font.Font(None, 100)
                 text = font.render(i[1], 1, (255, 255, 255))
                 self.screen.blit(text, [width - 580, height // 2 - 40])
