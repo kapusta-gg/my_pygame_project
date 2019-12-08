250e7ed3df8d1ae57d7408682e73dd5fccb5de1e
Bogdan <bogdashka04@gmail.com>
2019-12-08 11:20:07 +0300

    Добавил музыку

--- Main.py
+++ Main.py
@@ -6,7 +6,12 @@ pygame.init()
 size = width, height = GetSystemMetrics(0), GetSystemMetrics(1)
 screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
 backround_pos = [0, 0]
-backround_img = pygame.image.load('map.png')
+backround_img = pygame.image.load('Files_of_map/map_img.png')
+
+def play_music(music_path):
+    pygame.mixer.music.load(music_path)
+    pygame.mixer.music.set_volume(0.3)
+    pygame.mixer.music.play()
 
 running = True
 while running:
@@ -14,7 +19,7 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
-
+        play_music('Files_of_map/map_music.mp3')
         screen.blit(backround_img, backround_pos)
         pygame.display.flip()
 
