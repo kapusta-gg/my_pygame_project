082d11b2fe193927b91642424b096359c3dead64
Bogdan <bogdashka04@gmail.com>
2019-12-24 18:08:30 +0300

    Поменял отрисовывание кругов, сделал условие если пользователь нажал в круг

--- Map.py
+++ Map.py
@@ -20,8 +20,9 @@ class Map:
         map_on_play = load_level(self.txt)
         map_on_play = [map_on_play[i].split() for i in range(len(map_on_play))]
 
-        circles = pygame.sprite.Group()
+        circles = []
         panel = pygame.sprite.Group()
+        count = 0
 
         pygame.mixer.music.load(self.music)
         pygame.mixer.music.set_volume(0.3)
@@ -37,35 +38,30 @@ class Map:
                     if event.key == pygame.K_ESCAPE:
                         pygame.quit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    pass
+                    panel.update(event.pos)
+
 
             screen.blit(pygame.image.load(self.image), [0, 0])
-            sprite_draw(circles, panel, screen)
+            draw_on(circles, panel, screen)
             timing = int(pygame.mixer.music.get_pos() / 1000) / 10
-            position, flag = check_draw(position, timing)
+            position, flag = check_draw(position)
             if flag:
                 for i in map_on_play:
                     if position == float(i[0]):
                         draw_circle(int(i[2]), int(i[3]), i[1], panel, circles)
-                        #Stroke_panel(int(i[2]), int(i[3]), i[1], panel)
-                        #Ball(int(i[2]), int(i[3]), i[1], circles)
-            circles.draw(screen)
-            panel.update()
+                        count += 1
+            panel.update(None)
+            if count > len(panel):
+                count -= 1
+                del circles[0]
             clock.tick(60)
             pygame.display.flip()
 
 
-def sprite_draw(group1, group2, screen):
-    group1.draw(screen)
+def draw_on(group1, group2, screen):
     group2.draw(screen)
-
-
-class Ball(pygame.sprite.Sprite):
-    def __init__(self, x, y, color, sprites):
-        super().__init__(sprites)
-        self.image = pygame.Surface((2 * 50, 2 * 50), pygame.SRCALPHA, 32)
-        pygame.draw.circle(self.image, pygame.Color(color), (50, 50), 50)
-        self.rect = pygame.Rect(x, y, 2 * 50, 2 * 50)
+    for i in group1:
+        pygame.draw.circle(screen, pygame.Color(i[2]), (i[0] + 50, i[1] + 50), 50)
 
 
 class Stroke_panel(pygame.sprite.Sprite):
@@ -77,16 +73,21 @@ class Stroke_panel(pygame.sprite.Sprite):
         pygame.draw.circle(self.image, pygame.Color(color), (130, 130), 130, 1)
         self.rect = pygame.Rect(x - 80, y - 80, 500, 500)
 
-    def update(self):
-        self.image = pygame.Surface((6 * 50, 6 * 50), pygame.SRCALPHA, 32)
+    def update(self, args):
+        self.image = pygame.Surface((6 * 50 - self.reduce, 6 * 50 - self.reduce), pygame.SRCALPHA, 32)
         pygame.draw.circle(self.image, pygame.Color(self.color), (130, 130),
                            130 - self.reduce, 1)
         self.reduce += 2
         if self.reduce > 80:
             self.kill()
+        elif args is not None:
+            if self.rect.collidepoint(args):
+                self.kill()
 
 
-def check_draw(position, timing):
+
+
+def check_draw(position):
     timing = int(pygame.mixer.music.get_pos() / 1000) / 10
     if timing == position:
         return position, False
@@ -96,5 +97,4 @@ def check_draw(position, timing):
 
 def draw_circle(x, y, color, panel, circles):
     Stroke_panel(x, y, color, panel)
-    Ball(x, y, color, circles)
-
+    circles.append([x, y, color])
