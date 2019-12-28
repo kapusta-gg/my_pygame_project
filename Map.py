b6429b945c84f70c1726e0f6279d21010a713952
Bogdan <bogdashka04@gmail.com>
2019-12-28 15:30:55 +0300

    Добавил счетчик здоровья.

--- Map.py
+++ Map.py
@@ -1,5 +1,5 @@
 import pygame
-from win32api import GetSystemMetrics
+import os
 
 
 def load_level(filename):
@@ -7,6 +7,13 @@ def load_level(filename):
     with open(filename, 'r') as mapFile:
         level_map = [line.strip() for line in mapFile]
     return level_map
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname)
+    image = image.convert_alpha()
+    return image
 
 
 class Map:
@@ -26,10 +33,18 @@ class Map:
         combo = 0
         points = 0
         max_points = 0
+        hp = 100
         accuracy = 100
         tap_inf_list = []
+
         font1 = pygame.font.Font(None, 100)
         font2 = pygame.font.Font(None, 50)
+
+        sprite = pygame.sprite.Sprite()
+        heart_group = pygame.sprite.Group()
+        sprite.image = load_image('heart.png')
+        sprite.rect = sprite.image.get_rect()
+        heart_group.add(sprite)
 
         pygame.mixer.music.load(self.music)
         pygame.mixer.music.set_volume(0.3)
@@ -46,14 +61,17 @@ class Map:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     panel.update(event.pos, tap_inf_list)
 
-            text1 = font1.render('X' + str(combo), 1, (255, 255, 255))
-            text2 = font2.render(str(points), 1, (255, 255, 255))
-            text3 = font2.render(str(accuracy) + '%', 1, (255, 255, 255))
+            text1 = font1.render('X' + str(combo), 1, (3, 192, 60))
+            text2 = font2.render(str(points), 1, (3, 192, 60))
+            text3 = font2.render(str(accuracy) + '%', 1, (3, 192, 60))
+            text4 = font2.render(str(hp), 1, (3, 192, 60))
 
             screen.blit(pygame.image.load(self.image), [0, 0])
+            heart_group.draw(screen)
             screen.blit(text1, (0, 1020))
-            screen.blit(text2, (1650, 0))
-            screen.blit(text3, (1650, 50))
+            screen.blit(text2, (1780, 0))
+            screen.blit(text3, (1780, 50))
+            screen.blit(text4, (35, 0))
 
             draw_on(circles, panel, screen)
             position, flag = check_draw(position)
@@ -64,13 +82,18 @@ class Map:
                         count += 1
             panel.update(None, tap_inf_list)
             if count > len(panel):
-                print(tap_inf_list)
                 count -= 1
                 del circles[0]
                 if tap_inf_list == 0:
                     combo = 0
                 else:
                     combo += tap_inf_list[0][0]
+                if hp + tap_inf_list[0][3] > 100:
+                    hp = 100
+                elif hp + tap_inf_list[0][3] < 0:
+                    hp = 0
+                else:
+                    hp += tap_inf_list[0][3]
                 points += tap_inf_list[0][1]
                 max_points += tap_inf_list[0][2]
                 accuracy = int(points / max_points * 100)
@@ -113,8 +136,6 @@ class Stroke_panel(pygame.sprite.Sprite):
                     tap_inf(3, list)
 
 
-
-
 def check_draw(position):
     timing = int(pygame.mixer.music.get_pos() / 1000) / 10
     if timing == position:
@@ -129,12 +150,18 @@ def draw_circle(x, y, color, panel, circles):
 
 
 def tap_inf(inf, list):
-    print(list)
     if inf == 0:
-        list.append([0, 0, 300])
+        list.append([0, 0, 300, -20])
     elif inf == 1:
-        list.append([1, 300, 300])
+        list.append([1, 300, 300, 20])
     elif inf == 2:
-        list.append([1, 150, 300])
+        list.append([1, 150, 300, -10])
     else:
-        list.append([0, 0, 300])
+        list.append([0, 0, 300, -20])
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname)
+    image = image.convert_alpha()
+    return image
